2f7db8735ee1e816bde0b3f772d5e2ca502957d7
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2016-11-18 09:39:33 -0800

    api integration

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var bodyParser = require('body-parser');
 
 var fs = require('fs')
 var app = express();
+var lostStolen = require('mastercard-lost-stolen');
+var MasterCardAPI = lostStolen.MasterCardAPI;
 
 var dummyData = [];
-var dummyDataFiles = ['www/data/menu.json'];
+var dummyDataFiles = ['www/data/menu.json', 'www/data/account-number.json'];
 dummyDataFiles.forEach(function (file) {
     fs.readFile(file, 'utf8', function (err, data) {
         if (err) {
@@ -15,7 +17,25 @@ dummyDataFiles.forEach(function (file) {
     });
 });
 
-var useDummyData = true;
+var config = {
+    p12file: process.env.KEY_FILE || null,
+    p12pwd: process.env.KEY_FILE_PWD || 'keystorepassword',
+    p12alias: process.env.KEY_FILE_ALIAS || 'keyalias',
+    apiKey: process.env.API_KEY || null,
+    sandbox: process.env.SANDBOX || 'true',
+}
+
+var useDummyData = null == config.p12file;
+if (useDummyData) {
+    console.log('p12 file info missing, using dummy data')
+} else {
+    console.log('has p12 file info, using MasterCardAPI')
+    var authentication = new MasterCardAPI.OAuth(config.apiKey, config.p12file, config.p12alias, config.p12pwd);
+    MasterCardAPI.init({
+        sandbox: 'true' === config.sandbox,
+        authentication: authentication
+    });
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -39,6 +59,39 @@ app.post('/confirm', function (req, res) {
     if (useDummyData) {
         res.json({});
     } else {
+    }
+});
+
+app.post('/checkAccountNumber', function (req, res) {
+    if (useDummyData) {
+        if (null == dummyData[dummyDataFiles[1]][req.body.accountNumber]) {
+            res.json(dummyData[dummyDataFiles[1]].default);
+        } else {
+            res.json(dummyData[dummyDataFiles[1]][req.body.accountNumber]);
+        }
+    } else {
+        var requestData = {
+            "AccountInquiry": {
+                "AccountNumber": req.body.accountNumber
+            }
+        };
+        lostStolen.AccountInquiry.update(requestData, function (error, data) {
+            if (error) {
+                console.error("An error occurred");
+                console.dir(error, { depth: null });
+                res.json({
+                    MerchantCategoryCodeList: {
+                        MerchantCategoryCodeArray: {
+                            MerchantCategoryCode: []
+                        }
+                    }
+                });
+            }
+            else {
+                res.json(data);
+            }
+        });
+
     }
 });
 
